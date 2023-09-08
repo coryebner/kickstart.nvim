19b8c10c558698b9d04c53d944d8139cdb1d84b1
Cory Ebner <coryebner@gmail.com>
2023-09-08 17:16:59 -0600

    Added harpoon keybinds

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,4 +1,13 @@
 return {
- "ThePrimeagen/harpoon",
- dependencies = { "nvim-lua/plenary.nvim" },
+  "ThePrimeagen/harpoon",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+      { "<leader>ha", function() require("harpoon.mark").add_file() end, desc = "Harpoon [a]dd file" },
+      { "<C-f>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon quick menu" },
+      { "<leader>hj", function() require("harpoon.ui").nav_file(1) end, desc = "Harpoon nav file 1" },
+      { "<leader>hk", function() require("harpoon.ui").nav_file(2) end, desc = "Harpoon nav file 2" },
+      { "<leader>hl", function() require("harpoon.ui").nav_file(3) end, desc = "Harpoon nav file 3" },
+      { "<leader>h;", function() require("harpoon.ui").nav_file(4) end, desc = "Harpoon nav file 4" },
+  },
+
 }
