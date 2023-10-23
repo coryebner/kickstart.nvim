b778d20e30ad67a6c3f5b6b2ee2461c342cc7fee
Cory Ebner <coryebner@gmail.com>
2023-10-23 14:27:54 -0600

    Ensure CSharp treesitter is installed

--- init.lua
+++ init.lua
@@ -440,7 +440,7 @@ vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", { desc = 'Telescope 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'c_sharp' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
