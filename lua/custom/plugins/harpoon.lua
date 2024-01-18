138249b706d57ecf45a8a2745c04eeec5d4dc969
Cory Ebner <coryebner@gmail.com>
2024-01-18 16:20:06 -0700

    Updated Harpoon key bindings

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -2,12 +2,16 @@ return {
   "ThePrimeagen/harpoon",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-      { "<leader>ha", function() require("harpoon.mark").add_file() end, desc = "Harpoon [a]dd file" },
+      { "<C-M-a>", function() require("harpoon.mark").add_file() end, desc = "Harpoon [a]dd file" },
       { "<C-f>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon quick menu" },
-      { "<leader>hj", function() require("harpoon.ui").nav_file(1) end, desc = "Harpoon nav file 1" },
-      { "<leader>hk", function() require("harpoon.ui").nav_file(2) end, desc = "Harpoon nav file 2" },
-      { "<leader>hl", function() require("harpoon.ui").nav_file(3) end, desc = "Harpoon nav file 3" },
-      { "<leader>h;", function() require("harpoon.ui").nav_file(4) end, desc = "Harpoon nav file 4" },
+      { "<C-M-j>", function() require("harpoon.ui").nav_file(1) end, desc = "Harpoon nav file 1" },
+      { "<C-M-n>", function() require("harpoon.ui").nav_file(1) end, desc = "Harpoon nav file 1" },
+      { "<C-M-k>", function() require("harpoon.ui").nav_file(2) end, desc = "Harpoon nav file 2" },
+      { "<C-M-e>", function() require("harpoon.ui").nav_file(2) end, desc = "Harpoon nav file 2" },
+      { "<C-M-l>", function() require("harpoon.ui").nav_file(3) end, desc = "Harpoon nav file 3" },
+      { "<C-M-i>", function() require("harpoon.ui").nav_file(3) end, desc = "Harpoon nav file 3" },
+      { "<C-M-;>", function() require("harpoon.ui").nav_file(4) end, desc = "Harpoon nav file 4" },
+      { "<C-M-o>", function() require("harpoon.ui").nav_file(4) end, desc = "Harpoon nav file 4" },
   },
 
 }
